56376ee9204c7bd6dda7ce8e42522a3754384deb
Omega0707 <xuke.chen@jllov.com>
2026-07-21 16:17:03 +0800

    力扣练习43

--- lianxi02.py
+++ lianxi02.py
@@ -1,3 +1,8 @@
+'''
+给定两个以字符串形式表示的非负整数 num1 和 num2，返回 num1 和 num2 的乘积，它们的乘积也表示为字符串形式。
+
+注意：不能使用任何内置的 BigInteger 库或直接将输入转换为整数
+'''
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
         if num1=="0" or num2=="0":
